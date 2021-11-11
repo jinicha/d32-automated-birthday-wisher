4051c05eb146ae4c963676f42845a92da349fcc8
jinicha <jinichaa@gmail.com>
2021-11-10 20:25:25 -0800

    Complete sending email practice

---
+++ sendEmailPractice.py
@@ -0,0 +1,27 @@
+import smtplib
+import config
+
+gmail = config.gmail
+gmail_pw = config.gmail_pw
+yahoo = config.yahoo
+yahoo_pw = config.yahoo_pw
+
+# from gmail to yahoo
+with smtplib.SMTP("smtp.gmail.com") as connection:
+  connection.starttls()
+  connection.login(user=gmail, password=gmail_pw)
+  connection.sendmail(
+    from_addr=gmail,
+    to_addrs=yahoo,
+    msg="Subject: test1\n\nThis is test1 body."
+  )
+
+# from yahoo to gmail
+with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+  connection.starttls()
+  connection.login(user=yahoo, password=yahoo_pw)
+  connection.sendmail(
+    from_addr=yahoo,
+    to_addrs=gmail,
+    msg="Subject: test2\n\nThis is test2 body."
+  )
